b88de64658e6fc07e94cd49844a2af053488f914
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-31 00:07:10 +0200

    task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""LRUCache class"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """class LRUCache that inherits
+    from BaseCaching and is a caching system"""
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """method that puts a new item in the cache"""
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                self.queue.remove(key)
+                self.queue.append(key)
+                return
+            if len(self.cache_data) < BaseCaching.MAX_ITEMS:
+                self.cache_data[key] = item
+                self.queue.append(key)
+            else:
+                discard = self.queue.pop(0)
+                self.cache_data.pop(discard)
+                print("DISCARD: {}".format(discard))
+                self.cache_data[key] = item
+                self.queue.append(key)
+
+    def get(self, key):
+        """method that retrieves the item from the cache"""
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        return None

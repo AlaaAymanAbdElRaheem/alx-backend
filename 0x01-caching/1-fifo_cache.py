32f08649ac889f262ee59752b001140876a7ad28
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-30 21:01:22 +0200

    task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""FIFOCache class"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """class FIFOCache that inherits
+    from BaseCaching and is a caching system"""
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """method that puts a new item in the cache"""
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+            if len(self.cache_data) < BaseCaching.MAX_ITEMS:
+                self.cache_data[key] = item
+                self.queue.append(key)
+            else:
+                discard = self.queue.pop(0)
+                self.cache_data.pop(discard)
+                print("DISCARD: {}".format(discard))
+                self.cache_data[key] = item
+                self.queue.append(key)

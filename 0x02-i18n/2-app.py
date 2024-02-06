814790ee0c7ca68d3b171fa9abbe0645fc2373a5
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-02-06 17:36:12 +0200

    task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Instantiate the Babel object"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Config Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """index page"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
